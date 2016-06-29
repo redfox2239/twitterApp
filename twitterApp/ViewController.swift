1dde95be9bff936deb0c424f6e69d32053835bf7
原田礼朗 <rharada@mba-11aj-090.local>
2016-06-29 22:15:23 +0900

    Initial Commit

---
+++ twitterApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitterApp
+//
+//  Created by 原田　礼朗 on 2016/06/29.
+//  Copyright © 2016年 reo harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
